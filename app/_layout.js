649016bbc71fced6d5cfce17ac0c819ffaf321f6
Agafina <98034773+Agafina@users.noreply.github.com>
2024-05-07 22:19:34 +0100

    Implementation of the details page

--- app/_layout.js
+++ app/_layout.js
@@ -5,19 +5,19 @@ import * as SplashScreen from  'expo-splash-screen'
 
 SplashScreen.preventAutoHideAsync()
 const Layout = () => {
-    const [ fontLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-    })
-    const onLayerRootView = useCallback( async() => {
-        if(fontLoaded){
-            await SplashScreen.hideAsync()
-        }
-    },[fontLoaded])
-    
-    if(!fontLoaded) return null
-    return <Stack onLayout= {onLayerRootView}/>;
+    // const [ fontLoaded] = useFonts({
+    //     DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    //     DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    //     DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    // })
+    // const onLayerRootView = useCallback( async() => {
+    //     if(fontLoaded){
+    //         await SplashScreen.hideAsync()
+    //     }
+    // },[fontLoaded])
+
+    // if(!fontLoaded) return null
+    return <Stack />;
 }
  
 export default Layout;
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, ActivityIndicator,TouchableOpacity } from 'react-native'
+import { COLORS } from '../../../constants'
 import styles from './nearbyjobs.style'
+import { useRouter } from 'expo-router'
+import useFetch from '../../../hooks/useFetch'
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard'
 
-const Nearbyjobs = () => {
+const Popularjobs = () => {
+  const router = useRouter()
+
+  const { data, isLoading, error} = useFetch(
+    'search', {
+      query:'Backend Developer',
+      num_pages:1
+    }
+  )
+  console.log(data)
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size={25} color={COLORS.primary}/>
+        ): error ? (
+          <Text>Something went wrong</Text>
+        ): (
+          data?.map((job) => {
+           return (
+            <NearbyJobCard
+            job={job}
+            key={`nearby-job-${job?.job_id}`}
+            handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+          />
+           )
+          })
+        )}
+      </View>
     </View>
   )
 }
 
-export default Nearbyjobs
+export default Popularjobs
