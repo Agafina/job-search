43c4e4f9e0cc4d02581e609c0a6992f1ae9a3e16
Agafina <98034773+Agafina@users.noreply.github.com>
2024-05-09 01:18:33 +0100

    created tabs in the detailed page

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 import { SIZES } from '../../../constants'
 import styles from './tabs.style'
-
+const TabButton = ({name, activeTabs, onHandleSearchType}) => {
+  return (
+        <TouchableOpacity 
+      style={styles.btn(name, activeTabs)}
+      onPress={onHandleSearchType}
+      >
+        <Text>{name}</Text>
+      </TouchableOpacity>
+  )
+}
 const Tabs = ({tabs, activeTabs, setActiveTabs}) => {
   return (
     <View style={styles.container}>
       <FlatList 
        data={tabs}
        renderItem={({item}) => (
-        <TabButton />
+        <TabButton
+          activeTabs={activeTabs}
+          name={item}
+          onHandleSearchType ={() => setActiveTabs(item)}
+        />
        )}
+       horizontal
+       showsHorizontalScrollIndicator={false}
+       keyExtractor={item => item}
+       contentContainerStyle={{ columnGap: SIZES.small /2}}
       />
     </View>
   )
